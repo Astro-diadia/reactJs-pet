21febdd31d8e4fa1b835cd9550d5bf3bc151ca95
Astro-diadia <nicu.vbcrupnii@gmail.com>
2026-01-15 17:35:11 +0200

    playing with useeffect

---
+++ input-pet-proj/src/test.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const handleIncrement2 = () => {
+        setCount(count + 1);
+        setCount(count + 1);
+    };
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={handleIncrement2}>
+                Click me
+            </button>
+        </div>
+    );
+}
+
+export default Counter
